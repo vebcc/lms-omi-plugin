80fe7542c7bc8e8e4a96a73033a14211ae0c42f6
vebcc <vebcc08@gmail.com>
2023-03-31 00:07:00 +0200

    add translations

---
+++ lib/locale/pl/strings.php
@@ -0,0 +1,14 @@
+<?php
+$_LANG['Device with error list'] = 'Urządzenia z błędami';
+$_LANG['OltManager integration'] = 'Integracja z systemem OltManager';
+$_LANG['device'] = 'Komputer';
+$_LANG['networkDevice'] = 'Osprzęt sieciowy';
+$_LANG['problem:'] = 'Problem: ';
+$_LANG['invalid address:'] = 'Błędny adres: ';
+$_LANG['Object:'] = 'Urządzenie:';
+$_LANG['Object name:'] = 'Nazwa urządzenia:';
+$_LANG['colonMissing'] = 'Brak dwukropka w adresie onu';
+$_LANG['probablyCharacter'] = 'Prawdopodobnie podano znak zamiast nr w adresie onu';
+$_LANG['onuIdMissing'] = 'Brak ID onu w adresie';
+$_LANG['invalidVariant'] = 'Nieobsługiwany wariant zapisu adresu onu';
+$_LANG['oltShortNameMissing'] = 'Nie podano skróconej nazwy olta przed adresem onu. (np. TU_)';
