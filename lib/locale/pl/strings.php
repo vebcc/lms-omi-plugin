db891c3568aa8ea807a2456a2ac53a4d92fbd940
vebcc <vebcc08@gmail.com>
2023-04-07 19:30:41 +0200

    improvement | change permission name

--- lib/locale/pl/strings.php
+++ lib/locale/pl/strings.php
@@ -13,6 +13,6 @@ $_LANG['onuIdMissing'] = 'Brak ID onu w adresie';
 $_LANG['invalidVariant'] = 'Nieobsługiwany wariant zapisu adresu onu';
 $_LANG['oltShortNameMissing'] = 'Nie podano skróconej nazwy olta przed adresem onu. (np. TU_)';
 $_LANG['OltManager - full access'] = 'OltManager - pełny dostęp';
-$_LANG['OltManager - information review'] = 'OltManager - tylko odczyt i tylko do podstawowych modułów';
+$_LANG['OltManager - information review'] = 'OltManager - tylko odczyt';
 $_LANG['OltManager - LMS API full access'] = 'OltManager - pełny dostęp do modułu LMS API wtyczki';
 $_LANG['OltManager - OMI API full access'] = 'OltManager - pełny dostęp do modułu OMI API wtyczki';
